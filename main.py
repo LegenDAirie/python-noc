72b5333703482285aff98a1e3760db97afc0f54d
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-07-23 18:05:30 -0700

    just setting up a repository

---
+++ main.py
@@ -0,0 +1,20 @@
+from sketchy import Sketchy
+from ball import Ball
+from vector import Vector
+
+class MyDrawing(Sketchy):
+
+    def setup(self):
+        width, height = [600, 400]
+        self.ball = Ball(width, height)
+        self.size(width, height)
+
+    def update(self):
+
+        self.ball.update(self.mouseX, self.mouseY)
+
+    def draw(self, g):
+        g.background(1, 1, 1)
+        self.ball.draw(g)
+
+MyDrawing()
