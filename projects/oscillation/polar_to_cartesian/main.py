788a33a4d1a5ecb787390b3d81352491f0648735
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-07-31 16:50:08 -0700

    simple example of harmonic motion
    added an example of a periodic wave like motion, changed a class name in the file_template folder

--- projects/oscillation/polar_to_cartesian/main.py
+++ projects/oscillation/polar_to_cartesian/main.py
@@ -3,24 +3,27 @@ sys.path.append("../../../lib")
 
 from sketchy import Sketchy
 from vector import Vector
-from object1 import Ball
+from object1 import Square
+from object2 import Rectangle
 
 class MyDrawing(Sketchy):
 
     def setup(self):
         width, height = [600, 400]
+        self.line = Rectangle(width, height)
         self.balls = []
         for i in range(0, 1):
-            self.balls.append(Ball(width,height))
+            self.balls.append(Square(width,height))
             self.size(width, height)
 
     def update(self):
         for i in range(len(self.balls)):
             self.balls[i].update()
+        self.line.update()
 
     def draw(self, g):
         g.background(1, 1, 1)
         for i in range(len(self.balls)):
-            self.balls[i].draw(g)
-
+            self.balls[i].draw(g, 600, 400)
+        self.line.draw(g, 600, 400)
 MyDrawing()
