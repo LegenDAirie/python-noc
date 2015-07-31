4b01951ff5a43aa37c2d0ab7ea5e2dcb4c37937b
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-07-30 20:20:44 -0700

    objects face the the direction they move
    changed the angle so now every update it finds the angle and heading of the velocity vector using math.atan2() and applies it to the rectangles rotation so it faces the direction that it moves

---
+++ projects/oscillation/heading/main.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append("../../../lib")
+
+from sketchy import Sketchy
+from vector import Vector
+from ball import Ball
+
+class MyDrawing(Sketchy):
+
+    def setup(self):
+        width, height = [600, 400]
+        self.balls = []
+        for i in range(0, 3):
+            self.balls.append(Ball(width,height))
+        self.size(width, height)
+
+    def update(self):
+        self.mouse = Vector(self.mouseX, self.mouseY)
+        for i in range(len(self.balls)):
+            self.balls[i].update(self.mouse)
+
+    def draw(self, g):
+        g.background(1, 1, 1)
+        # g.push()
+        for i in range(len(self.balls)):
+            self.balls[i].draw(g, 600, 400)
+        # g.pop()
+
+MyDrawing()
