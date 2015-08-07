d7c389138f221bd418ed9c158fa6bae22260f0fb
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-08-07 14:25:31 -0700

    class for a class for a class
    made a system class that handles and keeps track of the particle system class that keeps track of the particles, this keeps main.py clean and makes managing easier

---
+++ projects/particle_systems/system_of_systems/object3.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append("../../../lib")
+
+from random import randint, random
+from vector import Vector
+from sketchy import Sketchy
+from physics import limit
+from object2 import ParticleSystem
+import math
+
+class SystemOfSystems(object):
+    def __init__(self):
+
+        self.particleSystems = []
+        self.counter = 60
+
+    def update(self, width, height, mouseX, mouseY):
+
+        self.counter -= 1
+        if self.counter <= 0:
+
+            # places the particle system at the beginning of the list
+            self.particleSystems.insert(0, ParticleSystem(width, height, mouseX, mouseY))
+            self.counter = 60
+
+        for i in range(len(self.particleSystems) -1, -1, -1):
+            self.particleSystems[i].update(mouseX, mouseY)
+            if len(self.particleSystems) > 4:
+                self.particleSystems.remove(self.particleSystems[i])
+
+    def draw(self, g):
+        for i in range(len(self.particleSystems)):
+            self.particleSystems[i].draw(g)
