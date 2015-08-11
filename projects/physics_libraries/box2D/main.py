77118d98e84b32b63606db545388f4aaa4c6af53
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-08-10 18:15:02 -0700

    set up directory and main file

---
+++ projects/physics_libraries/box2D/main.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("../../../lib")
+
+from sketchy import Sketchy
+
+
+class MyDrawing(Sketchy):
+
+    def setup(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, g):
+        pass
